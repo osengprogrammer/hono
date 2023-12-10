85417eb790ee77847ed7465abc5c1d8eb79b37ef
osengprogrammer <osengprogrammer@gmail.com>
2023-12-10 04:57:04 -0800

    Initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+const Dashboard = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className='text-2xl font-bold mb-5'>Dashboard</h1>
+      <p className='mb-5'>Welcome to the dashboard!</p>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes';
+import Header from '@/components/Header'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +20,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ClerkProvider
+       appearance={{
+        baseTheme: dark
+      }}
+      
+      >
+      <html lang="en">
+      
+        <body>
+        <Header />
+      
+            {children}
+         
+        </body>
+ 
+      </html>
+    </ClerkProvider>
     </html>
   )
 }
